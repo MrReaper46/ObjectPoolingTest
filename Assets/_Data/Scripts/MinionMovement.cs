2802fecfa026df798d804d196cb1faf9e9495ef0
MrReaper46 <129411428+MrReaper46@users.noreply.github.com>
2024-12-19 20:56:01 +0700

    update2

--- Assets/_Data/Scripts/MinionMovement.cs
+++ Assets/_Data/Scripts/MinionMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.ComponentModel;
 using UnityEngine;
 
 public class MinionMovement : MonoBehaviour
@@ -6,64 +6,25 @@ public class MinionMovement : MonoBehaviour
     [SerializeField] private Transform[] _checkPoints;
     [SerializeField] private int moveSpd = 5;
     [SerializeField] private int rotateSpd = 5;
-    [SerializeField] private int _currentCheckPointIndex = 0;
     [SerializeField] private Transform _currentCheckPoint;
     [SerializeField] private Transform _nextCheckPoint;
-    public int CurrentCheckPointIndex => _currentCheckPointIndex;
 
     private void Reset()
     {
-        this.LoadCheckPointCtrl();
+        LoadCheckPointCtrl();
     }
 
     private void OnEnable()
     {
-        this.StartCoroutine(MoveThroughCheckPoints());
-    }
-    private void FixedUpdate()
-    {
-        this.UpdateCheckPoint();
+        this._currentCheckPoint = _checkPoints[1];
+        this._nextCheckPoint = _checkPoints[0];
     }
     private void LoadCheckPointCtrl()
     {
         _checkPoints = GameObject.Find("CheckPointCtrl").GetComponent<CheckPoint_Ctrl>().CheckPoints;
-        _currentCheckPointIndex = 0;
-        _currentCheckPoint = _checkPoints[_currentCheckPointIndex];
-        _nextCheckPoint = _checkPoints[_currentCheckPointIndex + 1];
-        Debug.Log(_currentCheckPoint);
-        Debug.Log(_nextCheckPoint);
     }
-
-    private void UpdateCheckPoint()
+    private void FixedUpdate()
     {
-        if (transform.parent.position == _checkPoints[0].position || transform.parent.position == _checkPoints[_checkPoints.Length - 1].position)
-        {
 
-        }
     }
-
-    IEnumerator MoveThroughCheckPoints()
-    {
-        for (int i = 0; i < _checkPoints.Length; i++)
-        {
-            while (Vector3.Distance(transform.position, _checkPoints[i].position) > 0.1f)
-            {
-                transform.parent.position = Vector3.MoveTowards(transform.position, _checkPoints[i].position, moveSpd * Time.deltaTime);
-                yield return null;
-            }
-            yield return StartCoroutine(RotateAtCheckPoint());
-        }
-        yield return new WaitForSeconds(0.25f);
-    }
-    IEnumerator RotateAtCheckPoint()
-    {
-        Debug.Log("Start Rotation");
-        if (transform.parent.position == _checkPoints[0].position || transform.parent.position == _checkPoints[_checkPoints.Length - 1].position)
-        {
-            yield return null;
-        }
-        //transform.parent.rotation = Quaternion.RotateTowards(transform.parent.rotation, _checkPoints[i].rotation, rotateSpd * Time.deltaTime);
-        yield return new WaitForSeconds(0.25f);
-    }
-
 }
